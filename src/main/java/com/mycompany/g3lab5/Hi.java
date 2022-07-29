9506427f26d1e97592d1743c08ed7a5ac0509743
BuildTools <unconfigured@null.spigotmc.org>
2022-07-29 15:57:40 +0700

    Second Time Commit

--- src/main/java/com/mycompany/g3lab5/Hi.java
+++ src/main/java/com/mycompany/g3lab5/Hi.java
@@ -10,6 +10,6 @@ package com.mycompany.g3lab5;
  */
 public class Hi {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hi สวัสดี");
     }
 }

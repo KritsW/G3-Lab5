607ec67e7009b604b8570872abaede319c77ec7b
BuildTools <unconfigured@null.spigotmc.org>
2022-07-30 03:37:03 +0700

    Build Student and Staff

--- src/main/java/com/mycompany/g3lab5/Student.java
+++ src/main/java/com/mycompany/g3lab5/Student.java
@@ -8,37 +8,47 @@ package com.mycompany.g3lab5;
  *
  * @author LENOVO
  */
-public class Student _____________________ {
+public class Student extends Person{ 
+        //Instance variables = ตัวแปรของ Obj
     private String program;
     private int year;
     private double fee;
+        //Constructor
     public Student(String name, String address, String program, int year, double fee) {
-        __________________(name,address);
+        //เรียกจาก class แม่ ใช้ super
+        super(name,address);//Constructor call to super class (chaining)
         this.program = program;
         this.fee=fee;
         this.year=year;
          System.out.println("Invoke Student(String name, String address, String program, int year, double fee)");
     }
+    //Accessor Methods
     public String getProgram() {
         return program;
     }
+    //Mutator Medthod
     public void setProgram(String program) {
         this.program = program;
     }
+        //Accessor Methods
     public int getYear() {
         return year;
     }
+        //Mutator Medthod
     public void setYear(int year) {
         this.year = year;
     }
+        //Accessor Methods
     public double getFee() {
         return fee;
     }
+        //Mutator Medthod
     public void setFee(double fee) {
         this.fee = fee;
     }
+    @Override
     public String toString () {
-    return String.format("_________________________________", _____.toString(), program,year,fee);
+    return String.format("Student[%s, program=%s, year=%d, fee=%f", super.toString(), program,year,fee);
     }
 }
 
